313335732f0608c092b3cde8f201db11f8f2fd71
junyan zhang <junyan.zhang@here.com>
2018-10-08 13:55:49 -0400

    remove some test features

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"flag"
+	//"flag"
 	"net/http"
-
-	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	//"k8s.io/client-go/tools/clientcmd"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// "k8s.io/client-go/kubernetes"
 )
 
 var (
@@ -30,23 +30,23 @@ func main() {
 	metricsRegistry := prometheus.NewRegistry()
 	metricsRegistry.Register(requestCounterVec)
 	// creates the in-cluster config
-	configFile := flag.String("kubeconfig", "c:\\dev\\go\\src\\org.junyan\\test\\kube-api-server.conf", "(optional) absolute path to the kubeconfig file")
-	config, err := clientcmd.BuildConfigFromFlags("", *configFile)
-	if err != nil {
-		panic(err.Error())
-	}
+	// configFile := flag.String("kubeconfig", "c:\\dev\\go\\src\\org.junyan\\test\\kube-api-server.conf", "(optional) absolute path to the kubeconfig file")
+	// config, err := clientcmd.BuildConfigFromFlags("", *configFile)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
 
 	// creates the clientset
-	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	kubeClient = clientSet
+	// clientSet, err := kubernetes.NewForConfig(config)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// kubeClient = clientSet
 
 	router := gin.Default()
 	router.GET("/get", get200)
 	router.GET("/metrics", gin.WrapH(promhttp.HandlerFor(metricsRegistry, promhttp.HandlerOpts{ErrorHandling: promhttp.HTTPErrorOnError})))
-	router.GET("/test", testKubeSecretes)
+	//router.GET("/test", testKubeSecretes)
 	router.Run(":8888")
 }
 
@@ -56,19 +56,19 @@ func get200(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"status": "OK!"})
 }
 
-func testKubeSecretes(c *gin.Context) {
-	// pods, err := kubeClient.CoreV1().Pods("default").List(metav1.ListOptions{})
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	secrets, err := kubeClient.CoreV1().Secrets("default").Get("dbadmin.grafana", metav1.GetOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"endpoint": string(secrets.Data["endpoint"][:]),
-		"port":     string(secrets.Data["port"][:]),
-		"username": string(secrets.Data["username"][:]),
-		"password": string(secrets.Data["password"][:]),
-	})
-}
+// func testKubeSecretes(c *gin.Context) {
+// 	// pods, err := kubeClient.CoreV1().Pods("default").List(metav1.ListOptions{})
+// 	// if err != nil {
+// 	// 	panic(err.Error())
+// 	// }
+// 	secrets, err := kubeClient.CoreV1().Secrets("default").Get("dbadmin.grafana", metav1.GetOptions{})
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	c.IndentedJSON(http.StatusOK, gin.H{
+// 		"endpoint": string(secrets.Data["endpoint"][:]),
+// 		"port":     string(secrets.Data["port"][:]),
+// 		"username": string(secrets.Data["username"][:]),
+// 		"password": string(secrets.Data["password"][:]),
+// 	})
+// }
